15dcbfdccb768bdd027a5bb40e1d309311f71d9f
leo-ortiz22 <101903651+leo-ortiz22@users.noreply.github.com>
2022-06-20 15:12:59 -0500

    contacts

---
+++ contacts/contacts.java
@@ -0,0 +1,81 @@
+package contacts;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class contacts {
+
+
+    public static void main(String[] args) {
+
+
+        String directory = "data";
+        String filename = "contacts.txt";
+
+
+        try {
+            Path dataDirectory = Paths.get(directory);
+            Path dataFile = Paths.get(directory, filename);
+
+            if(Files.notExists(dataDirectory)) {
+                System.out.println("Creating new directory named: " + dataDirectory);
+
+                Files.createDirectories(dataDirectory);
+            }
+
+            if(!Files.exists(dataFile)) {
+                System.out.println("Your new file has been created!");
+
+                Files.createFile(dataFile);
+            }
+
+            List<String> dataText = new ArrayList<>();
+            dataText.add("Athena");
+            dataText.add("Medusa");
+            dataText.add("Hades");
+            dataText.add("Aphrodite");
+            dataText.add("Artemis");
+            dataText.add("Ares");
+            dataText.add("Zeus");
+
+            if(Files.exists(dataFile)) {
+                System.out.println("Writing to info.txt file!");
+                Files.write(dataFile, dataText);
+                Files.write(dataFile, Arrays.asList("Apollo", "Gaia"), StandardOpenOption.APPEND);
+
+                System.out.println(dataText);
+            }
+        } catch(IOException iox) {
+            iox.printStackTrace();
+        }
+
+
+        try {
+            List<String> fileData = Files.readAllLines(Paths.get("data", "contacts.txt"));
+            System.out.println(fileData);
+
+            List<String> newList = new ArrayList<>();
+
+            for (String god : fileData) {
+                if(god.equals("Deimos")) {
+                    continue;
+                }
+                newList.add(god);
+            }
+            Files.write(Paths.get("data", "contacts.txt"), newList);
+            fileData = Files.readAllLines(Paths.get("data", "contacts.txt"));
+            for (String line: fileData) {
+                System.out.println(line);
+            }
+
+        } catch( IOException iox) {
+            iox.printStackTrace();
+        }
+    }
+}
+
---
+++ contacts/contactsApp.java
@@ -0,0 +1,40 @@
+package contacts;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class contactsApp {
+    public static void main(String[] args) {
+
+        System.out.println("1. View contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit");
+        System.out.println("Enter an option (1, 2, 3, 4, or 5)");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userChoice = scanner.nextLine();
+
+        Path contacts = Paths.get("data","contacts.txt");
+
+        List<String> contactsList = new ArrayList<>();
+
+
+        try{
+          contactsList = Files.readAllLines(contacts);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for(String contact: contactsList){
+            System.out.println(contact);
+        }
+    }
+
+}
